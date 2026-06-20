0ab08e0ccfb92c10cc84100a157eb89e1cbf5528
Aditya Sharma <adirockz004@gmail.com>
2026-06-20 22:49:33 +0530

    feat: initialize expo-router stack layout and home screen component

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Hi My Name is Aditya Sharma</Text>
+      <Text>This is my new app</Text>
+    </View>
+  );
+}
